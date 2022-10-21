574e40925c9ebef6578394b988105d50ae3fdf86
hypopyon <hypopyon@protonmail.com>
2022-10-20 20:11:49 -0500

    Preliminary UI work

---
+++ Scripts/Main.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+using System.Data;
+
+namespace Delve;
+
+public partial class Main : Node {
+
+	Map map = null!;
+	CanvasLayer uiLayer = null!;
+	Player player = null!;
+
+	uint resources, tradeGoods;
+	
+	public override void _Ready() {
+		if (GetNode("Map") is Map getMap)
+			map = getMap;
+		else throw new Exception();
+		
+		if (GetNode("UILayer") is CanvasLayer getUiLayer)
+			uiLayer = getUiLayer;
+		else throw new Exception();
+		
+		if (GetNode("Player") is Player getPlayer)
+			getPlayer = player;
+		else throw new Exception();
+	}
+
+	public override void _Process(double delta) {
+		{ if (uiLayer.GetNode("MainMargins/Inventory/Resources/Label") is Label label) {
+			label.Text = resources.ToString();
+			label.QueueRedraw();
+		} }
+		{ if (uiLayer.GetNode("MainMargins/Inventory/TradeGoods/Label") is Label label) {
+			label.Text = tradeGoods.ToString();
+			label.QueueRedraw();
+		} }
+		GD.Print(map.GetTile(0, 0));
+	}
+}
